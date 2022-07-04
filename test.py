ad052c13e2d202441dc6b28ff69a5789f4982e0a
langshuang01 <langshuang01@baidu.com>
2022-07-04 13:49:20 +0800

    go on

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 from manim import *
 
-class SquareAndCircle(Scene):
+class TestScene(MovingCameraScene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        t1 = Tex("{{x}}")
+        t2 = Tex("{{x}} - {{x}}")
+        # VGroup(t1, t2)\
+        #     .scale(3)\
+        #     .arrange(DOWN)
 
-        square = Square()  # create a square
-        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
-
-        square.next_to(circle, RIGHT, buff=0.5)  # set the position
-        self.play(Create(circle), Create(square))  # show the shapes on screen
+        self.add(t1)
+        self.wait()
+        self.play(
+            TransformMatchingTex(t1, t2),
+            run_time=4
+        )
+        self.wait()
